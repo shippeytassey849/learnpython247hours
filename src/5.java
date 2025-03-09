84a6a7e33fe6414e8b334ec20c1cdfd3619f0c3e
shippeytassey849 <shippeytassey849@gmail.com>
2025-03-09 08:46:40 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        System.out.println("The random number is: " + number);
+    }
+}
